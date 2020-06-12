ad6108ea579738994108fcc4d91d09ee279a14df
Chris Ha <chunghha@users.noreply.github.com>
2020-06-12 07:36:17 -0500

    initial commit

---
+++ src/greeter/client.rs
@@ -0,0 +1,27 @@
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
+use greet::greeter_client::GreeterClient;
+use greet::GreetRequest;
+
+pub mod greet {
+    tonic::include_proto!("greet");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    pretty_env_logger::init();
+
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(GreetRequest {
+        name: "Tonic".into(),
+    });
+
+    let response = client.hello(request).await?;
+
+    info!("RESPONSE={:?}", response);
+
+    Ok(())
+}
---
+++ src/greeter/server.rs
@@ -0,0 +1,43 @@
+extern crate pretty_env_logger;
+#[macro_use]
+extern crate log;
+
+use tonic::{transport::Server, Request, Response, Status};
+
+use greet::greeter_server::{Greeter, GreeterServer};
+use greet::{GreetReply, GreetRequest};
+
+pub mod greet {
+    tonic::include_proto!("greet");
+}
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn hello(&self, request: Request<GreetRequest>) -> Result<Response<GreetReply>, Status> {
+        info!("Got a request: {:?}", request);
+
+        let reply = greet::GreetReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    pretty_env_logger::init();
+
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+}
